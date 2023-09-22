438d215cbbb15168a242aebcfec800ceb6a610d8
techyard23 <145033247+techyard23@users.noreply.github.com>
2023-09-22 22:01:36 +0530

    Add files via upload

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,21 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {NgIf} from '@angular/common';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -6,5 +23,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  user = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    address: '',
+    city: '',
+    state: '',
+    country: '',
+    zip: '',
+    dob: '',
+  };
+  selectedDate: Date | undefined;
+  usernameFormControl = new FormControl('');
+  public submit_status: string | undefined ;
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  matcher = new MyErrorStateMatcher();
+  onSubmit() {
+    // Here, you can handle form submission, for example, sending data to a server
+    this.submit_status = 'Registration successful!';
+  }
 }
