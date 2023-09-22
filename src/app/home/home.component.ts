0cbc18bae987cb1d899f5876ffa663003da11155
Amlan Ghosh <amlan.ghosh01@gmail.com>
2023-09-22 18:39:34 +0530

    sab checkin in hai baba

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  pincodeFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^[0-9]{6}$/),
+  ]);
 }
